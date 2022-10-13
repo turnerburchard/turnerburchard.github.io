0168fbbe6538d73f4caf8b2fc057f7f5d76df8b5
Turner <tburchard4@gmail.com>
2022-10-12 23:18:31 -0600

    remake

---
+++ fun.js
@@ -0,0 +1,9 @@
+
+const box = document.getElementById('landingButton');
+
+const landing = document.getElementById('landing');
+
+box.addEventListener('click', function handleClick(event) {
+    // event.target.remove();
+    landing.remove()
+});
