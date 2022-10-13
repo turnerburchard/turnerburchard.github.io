87b66be77b127c2f468c39f3cbef89083bfa1297
Turner <tburchard4@gmail.com>
2022-10-13 15:23:17 -0600

    landing page

--- fun.js
+++ fun.js
@@ -1,9 +1,13 @@
 
-const box = document.getElementById('landingButton');
-
+const landingButton = document.getElementById('landingButton');
 const landing = document.getElementById('landing');
 
-box.addEventListener('click', function handleClick(event) {
-    // event.target.remove();
-    landing.remove()
+landing.addEventListener('click', function handleClick(event) {
+    changeScroll();
+    landing.remove();
 });
+
+function changeScroll(){
+    const body = document.getElementById('body')
+    body.style.overflow = "auto";
+}
