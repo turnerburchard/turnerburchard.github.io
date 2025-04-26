21c50d787d348e93466be6972b7dbf9c87eb3ae2
turnerburchard <tburchard4@gmail.com>
2025-04-25 22:14:26 -0600

    new photos

--- js/gallery.js
+++ js/gallery.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 const galleries = {
     photos: {
         path: 'files/photos/',
-        images: ['20230923-DSC_0700-Enhanced-NR.jpg', '20230929-DSC_0745.jpg', '20240310-DSC07552 Copy.JPG', '20240310-DSC07554.jpg', '20240310-DSC07557.jpg', '20240311-DSC08259.jpg', '20240721-DSC00967.jpg', '20240721-DSC01024.jpg', '20240721-DSC01059.jpg', '20241013-DSC09438-Enhanced-NR.jpg', 'Burchard_landscape-0450.JPG', 'photo1.jpg', 'photo10.jpg', 'photo11.jpg', 'photo12.jpg', 'photo14.jpg', 'photo20.jpg', 'photo8.jpg'],
+        images: ['20230923-DSC_0700-Enhanced-NR.jpg', '20230929-DSC_0745.jpg', '20240310-DSC07363.jpeg', '20240310-DSC07552 Copy.JPG', '20240310-DSC07554.jpg', '20240310-DSC07557.jpg', '20240311-DSC08259.jpg', '20240721-DSC00967.jpg', '20240721-DSC01024.jpg', '20240721-DSC01059.jpg', '20241013-DSC09438-Enhanced-NR.jpg', 'Burchard_landscape-0450.JPG', 'IMG_2657.jpeg', 'photo1.jpg', 'photo10.jpg', 'photo11.jpg', 'photo12.jpg', 'photo14.jpg', 'photo20.jpg', 'photo8.jpg'],
         containerId: '#photos .gallery-container'
     },
     wildlife: {
@@ -13,12 +13,12 @@ const galleries = {
     },
     me: {
         path: 'files/me/',
-        images: ['20240310-DSC07164-Enhanced-NR.jpg', '20240314-DSC01675.jpg', '20240316-DSC02406.jpg', '20240814-DSC00838.jpg', '20240814-DSC00891.jpg', '20240814-DSC01267.jpg', 'DSC07126-2 Copy.JPG', 'DSC07164.jpg', 'DSC07494.jpg', 'photo15.jpg', 'photo18.jpg', 'tetons-0333-2.jpg'],
+        images: ['20240310-DSC07164-Enhanced-NR.jpg', '20240314-DSC01675.jpg', '20240316-DSC02406.jpg', '20240814-DSC00838.jpg', '20240814-DSC00891.jpg', '20240814-DSC01267.jpg', 'DSC07126-2 Copy.JPG', 'DSC07164.jpg', 'DSC07494.jpg', 'IMG_7909.jpeg', 'photo15.jpg', 'photo18.jpg', 'tetons-0333-2.jpg'],
         containerId: '#me .gallery-container'
     },
     art: {
         path: 'files/art/',
-        images: ['photo16.jpg', 'photo17.jpg', 'photo3.jpg', 'photo4.jpg', 'photo5.jpg', 'photo7.jpg'],
+        images: ['IMG_3436.jpeg', 'IMG_3525.jpeg', 'photo16.jpg', 'photo17.jpg', 'photo3.jpg', 'photo4.jpg', 'photo5.jpg', 'photo7.jpg'],
         containerId: '#art .gallery-container'
     }
 };
