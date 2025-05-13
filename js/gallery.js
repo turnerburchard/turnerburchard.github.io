39765e9cb0c139aa88f9499eda7ff199d7eeac95
turnerburchard <tburchard4@gmail.com>
2025-05-13 14:10:01 -0600

    photos

--- js/gallery.js
+++ js/gallery.js
@@ -3,17 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
 const galleries = {
     photos: {
         path: 'files/photos/',
-        images: ['20230923-DSC_0700-Enhanced-NR.jpg', '20230929-DSC_0694-2.jpg', '20230929-DSC_0745.jpg', '20240310-DSC07363.jpeg', '20240310-DSC07552 Copy.JPG', '20240310-DSC07554.jpg', '20240310-DSC07557.jpg', '20240311-DSC08259.jpg', '20240721-DSC00967.jpg', '20240721-DSC01024.jpg', '20240721-DSC01059.jpg', '20241013-DSC09438-Enhanced-NR.jpg', 'Burchard_landscape-0450.JPG', 'IMG_2657.jpeg', 'photo1.jpg', 'photo10.jpg', 'photo11.jpg', 'photo12.jpg', 'photo14.jpg', 'photo20.jpg', 'photo8.jpg', 'tetons-0197-2.jpg', 'tetons-0911-Enhanced-NR.jpg'],
+        images: ['20230923-DSC_0700-Enhanced-NR.jpg', '20230929-DSC_0694-2.jpg', '20230929-DSC_0745.jpg', '20240310-DSC07363.jpeg', '20240310-DSC07552 Copy.JPG', '20240310-DSC07554.jpg', '20240310-DSC07557.jpg', '20240311-DSC08259.jpg', '20240721-DSC00967.jpg', '20240721-DSC01024.jpg', '20240721-DSC01059.jpg', '20240908-DSC07028.jpg', '20241013-DSC09438-Enhanced-NR.jpg', 'Burchard_landscape-0450.JPG', 'IMG_2657.jpeg', 'photo1.jpg', 'photo10.jpg', 'photo11.jpg', 'photo12.jpg', 'photo14.jpg', 'photo20.jpg', 'photo8.jpg', 'tetons-0197-2.jpg', 'tetons-0911-Enhanced-NR.jpg'],
         containerId: '#photos .gallery-container'
     },
     wildlife: {
         path: 'files/wildlife/',
-        images: ['20231007-DSC_0851-Enhanced-NR-2.jpg', '20240310-DSC07647-Enhanced-NR-3.jpg', '20240310-DSC07694.jpg', '20240310-DSC07701-Enhanced-NR.jpg', '20240310-DSC07848.jpg', '20240311-DSC08814-Enhanced-NR.jpg', '20240311-DSC08990.jpg', '20240313-DSC09842.jpg', '20240721-DSC00950-Enhanced-NR.jpg', '20240812-DSC00676.jpg', '20240812-DSC00690.jpg', '20240819-DSC03782.jpg', '20240819-DSC04129.jpg', '20240819-DSC04373.jpg', '20240819-DSC04457.jpg', '20240819-DSC04533.jpg', '20240819-DSC04725.jpg', 'DSC_0311.JPG', 'DSC_0458.JPG', 'photo13.jpg', 'photo19.jpg', 'photo2.jpg', 'photo6.jpg', 'photo9.jpg', 'tetons-0167-Enhanced-NR.jpg'],
+        images: ['20231007-DSC_0851-Enhanced-NR-2.jpg', '20240310-DSC07647-Enhanced-NR-3.jpg', '20240310-DSC07694.jpg', '20240310-DSC07701-Enhanced-NR.jpg', '20240310-DSC07848.jpg', '20240311-DSC08814-Enhanced-NR.jpg', '20240311-DSC08990.jpg', '20240313-DSC09842.jpg', '20240721-DSC00950-Enhanced-NR.jpg', '20240812-DSC00676.jpg', '20240812-DSC00690.jpg', '20240819-DSC03782.jpg', '20240819-DSC04129.jpg', '20240819-DSC04373.jpg', '20240819-DSC04457.jpg', '20240819-DSC04533.jpg', '20240819-DSC04725.jpg', '20240908-DSC06709.jpg', 'DSC_0311.JPG', 'DSC_0458.JPG', 'photo13.jpg', 'photo19.jpg', 'photo2.jpg', 'photo6.jpg', 'photo9.jpg', 'tetons-0167-Enhanced-NR.jpg'],
         containerId: '#wildlife .gallery-container'
     },
     me: {
         path: 'files/me/',
-        images: ['20240310-DSC07164-Enhanced-NR.jpg', '20240314-DSC01675.jpg', '20240316-DSC02406.jpg', '20240814-DSC00838.jpg', '20240814-DSC00891.jpg', '20240814-DSC01267.jpg', '20240928-DSC07457.jpg', 'DSC07126-2 Copy.JPG', 'DSC07164.jpg', 'DSC07494.jpg', 'IMG_7909.jpeg', 'photo15.jpg', 'photo18.jpg', 'tetons-0333-2.jpg'],
+        images: ['20240310-DSC07164-Enhanced-NR.jpg', '20240314-DSC01675.jpg', '20240316-DSC02406.jpg', '20240814-DSC00838.jpg', '20240814-DSC00891.jpg', '20240814-DSC01267.jpg', '20240908-DSC06537.jpg', '20240914-DSC07251.jpg', '20240928-DSC07457.jpg', 'DSC07126-2 Copy.JPG', 'DSC07164.jpg', 'DSC07494.jpg', 'IMG_7909.jpeg', 'photo15.jpg', 'photo18.jpg', 'tetons-0333-2.jpg'],
         containerId: '#me .gallery-container'
     },
     art: {
